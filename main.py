0ed1bdc812a5131ebcec600136c8ae420afb45c2
dheeraj080 <kambledheerajkumar@gmail.com>
2026-01-16 10:14:05 +0530

    MVP FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
@@ -9,7 +9,7 @@ load_dotenv()
 app = FastAPI()
 
 DATABASE_URL = (os.getenv('SUPABASE_URL'))
-#
+
 try:
     engine = create_engine(DATABASE_URL)
     connection = engine.connect()
@@ -17,35 +17,37 @@ try:
     connection.close()
 except:
     print("DB FAILED")
-
-amount = float(input("Enter amount to convert: "))
-from_currerncy = input("Enter first code: ").upper()
-to_currency = input("Enter second code: ").upper()
-
-try:
-    with engine.connect() as connection:
-        # 1. Define the query with named parameters
-        query = text("SELECT currency_codes, rate FROM exchange_rates WHERE currency_codes IN (:c1, :c2)")
-        result = connection.execute(query, {"c1": from_currerncy, "c2": to_currency})
-        # 2. Pass the dictionary as the second argument
-        # This resolves the 'InvalidRequestError'
-        
-        rates = {row.currency_codes: row.rate for row in result}
-        
-        if from_currerncy in rates and to_currency in rates:
-            rate_from = rates[from_currerncy]
-            rate_to = rates[to_currency]
+  
+@app.get("/convert")
+def convert(from_currency:str, to_currency:str, amount:float):
+    
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT currency_codes, rate FROM exchange_rates WHERE currency_codes IN (:c1, :c2)")
+            result = connection.execute(query, {"c1": from_currency, "c2": to_currency})
             
-            converted_amount = amount * (rate_to/rate_from)
-            
-            print(f"---Conversion---")
-            print(f"{amount}{from_currerncy} = {converted_amount:.2f}{to_currency}")
+            rates = {row.currency_codes: row.rate for row in result}
         
-        for row in result:
-            print(f"Code: {row.currency_codes}, Rate: {row.rate}")
-
-except Exception as e:
-    print("Error:", e)
-
+        if from_currency not in rates or to_currency not in rates:
+            raise HTTPException(status_code=404, detail="One or both currency codes not found")
+    
+        rate_from = rates[from_currency]
+        rate_to = rates[to_currency]
+            
+        converted_amount = amount * (rate_to/rate_from)
+        
+        return {
+            "amount": amount,
+            "from": from_currency,
+            "to": to_currency,
+            "result": round(converted_amount, 2),
+            "rate": round(rate_to / rate_from, 4)
+        }
+    
+    
+    
+    except Exception as e:
+        print("Error:", e)
+        
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
